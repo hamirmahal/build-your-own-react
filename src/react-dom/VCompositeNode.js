3202120e5bc3af58bb5b8034407d3f3527954efa
Hamir Mahal <hamirmahal@gmail.com>
2021-01-10 17:57:58 -0800

    Complete task 5. Functional components and props
    This commit completes task 5. Functional components and props (https://github.com/hamirmahal/build-your-own-react#5-functional-components-and-props).

--- src/react-dom/VCompositeNode.js
+++ src/react-dom/VCompositeNode.js
@@ -1,11 +1,37 @@
 import { instantiateVNode } from './index';
 
 export default class VCompositeNode {
-    constructor(reactElement) {}
+    /**
+     * initializes this
+     * VCompositeNode instance with
+     * a class property that is a
+     * React element.
+     * @param {object} reactElement 
+     * is an object denoting
+     * a React element. Its type
+     * is a React functional
+     * component.
+     */
+    constructor(reactElement) {
+        this.reactElement = reactElement;
+    }
 
     getPublicInstance() {}
 
     update() {}
 
-    mount() {}
+    /**
+     * calls this VCompositeNode's
+     * React element's type, which is a
+     * React functional component, with
+     * this VCompositeNode's React
+     * element's props object passed
+     * as a parameter argument.
+     * @returns {*} a mounted Node
+     */
+    mount() {
+        const renderedComponent = this.reactElement.type(this.reactElement.props);
+        const vNode = instantiateVNode(renderedComponent);
+        return vNode.mount();
+    }
 }
--- src/react-dom/index.js
+++ src/react-dom/index.js
@@ -1,7 +1,14 @@
 import VCompositeNode from './VCompositeNode';
 import VDomNode from './VDomNode';
 
+/**
+ * @returns {VCompositeNode | VDomNode} a
+ * virtual node instantiated with reactElement
+ * @param {*} reactElement 
+ */
 export function instantiateVNode(reactElement) {
+    if (typeof reactElement?.type === 'function')
+        return new VCompositeNode(reactElement);
     return new VDomNode(reactElement);
 }
 
