f950605184fd260f4e89e408930aa3eb3e995650
Hamir Mahal <hamirmahal@gmail.com>
2021-01-11 00:40:39 -0800

    Do 8.Attributes and refactor mount for readability
    This commit completes the eighth task in https://github.com/hamirmahal/build-your-own-react, which has the title "8. Attributes" (https://github.com/hamirmahal/build-your-own-react#8-attributes).

--- src/react-dom/VDomNode.js
+++ src/react-dom/VDomNode.js
@@ -5,6 +5,34 @@ export default class VDomNode {
         this.reactElement = reactElement;
     }
 
+    /**
+     * adds the props from this
+     * VDomNode's React element to an
+     * Element.
+     * @param {Element} domNode is the
+     * element to which we are adding
+     * this VDomNode's React element's
+     * props.
+     * @returns {void} nothing
+     */
+    addPropsFromThisReactElementTo(domNode) {
+        const { props } = this.reactElement;
+        Object.entries(props).forEach(([propKey, propValue]) => {
+            // 6. https://github.com/hamirmahal/build-your-own-react#6-classname
+            // Set the className attribute of the domNode Element.
+            if (propKey === 'className')
+                domNode.className = propValue;
+            // 7. https://github.com/hamirmahal/build-your-own-react#7-inline-styles
+            // Set the style attribute of domNode.
+            else if (propKey === 'style')
+                Object.entries(props.style).forEach(([styleProperty, styleValue]) => {
+                    domNode.style[styleProperty] = styleValue;
+                });
+            else
+                domNode.setAttribute(propKey, propValue);
+        });
+    }
+
     getPublicInstance() {}
 
     update() {}
@@ -18,15 +46,9 @@ export default class VDomNode {
         if (typeof this.reactElement === 'number' || typeof this.reactElement === 'string')
             return document.createTextNode(this.reactElement);
         const domNode = document.createElement(this.reactElement.type);
-        // 6. https://github.com/hamirmahal/build-your-own-react#6-classname
-        // Set the className attribute of the domNode Element.
-        domNode.className = this.reactElement.props.className;
-        // 7. https://github.com/hamirmahal/build-your-own-react#7-inline-styles
-        // Set the style attribute of domNode.
-        const { style } = this.reactElement.props;
-        style && Object.entries(style).forEach(([styleProperty, styleValue]) => {
-            domNode.style[styleProperty] = styleValue;
-        });
+        // 8. https://github.com/hamirmahal/build-your-own-react#8-attributes
+        // Add attributes to domNode.
+        this.addPropsFromThisReactElementTo(domNode);
         const children = getChildrenAsArray(this.reactElement.props);
         const vDomNodeChildren = children.map(instantiateVNode);
         vDomNodeChildren.forEach(vDomNodeChild => {
