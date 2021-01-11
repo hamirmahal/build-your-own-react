5069928fe316f21d250337934a1f90e2814cc1d3
Hamir Mahal <hamirmahal@gmail.com>
2021-01-10 18:07:58 -0800

    Add className attribute support for 6. className
    This commit completes 6. className (https://github.com/hamirmahal/build-your-own-react#6-classname).

--- src/react-dom/VDomNode.js
+++ src/react-dom/VDomNode.js
@@ -9,12 +9,18 @@ export default class VDomNode {
 
     update() {}
 
+    /**
+     * @returns {Element | Text} a non-virtual DOM node
+     */
     mount() {
         if (this.reactElement === null || this.reactElement === undefined)
             return document.createTextNode('');
         if (typeof this.reactElement === 'number' || typeof this.reactElement === 'string')
             return document.createTextNode(this.reactElement);
         const domNode = document.createElement(this.reactElement.type);
+        // 6. https://github.com/hamirmahal/build-your-own-react#6-classname
+        // Set the className attribute of the domNode Element.
+        domNode.className = this.reactElement.props.className;
         const children = getChildrenAsArray(this.reactElement.props);
         const vDomNodeChildren = children.map(instantiateVNode);
         vDomNodeChildren.forEach(vDomNodeChild => {
