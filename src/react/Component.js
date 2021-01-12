6bc85a3880bf57c165b6b687e5b9b375e089e487
Hamir Mahal <hamirmahal@gmail.com>
2021-01-11 20:32:19 -0800

    Complete task 10. React class components
    This commit completes task 10. React class components (https://github.com/hamirmahal/build-your-own-react#10-react-class-components).

--- src/react/Component.js
+++ src/react/Component.js
@@ -1,11 +1,30 @@
 import ReactDOM from '../react-dom';
 
 class Component {
-    constructor(props) {}
+    // 10. https://github.com/hamirmahal/build-your-own-react#10-react-class-components
+    // Begin implementation of React class components.
+    constructor(props) {
+        this.props = props
+    }
 
     setState(state) {}
 
-    render() {}
+    // 10. https://github.com/hamirmahal/build-your-own-react#10-react-class-components
+    // Begin implementation of React class components.
+    /**
+     * is the only required
+     * method in a class component.
+     * It should return React
+     * elements.
+     * @returns a React element
+     */
+    render() {
+        throw new Error("The Component class does not extend React.Component.");
+    }
 }
+
+// 10. https://github.com/hamirmahal/build-your-own-react#10-react-class-components
+// Begin implementation of React class components.
+Component.prototype.isReactComponent = true;
 
 export default Component;
