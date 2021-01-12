ac06de5e0a99e41f8e3eb0642c4d857d6ac7e262
Hamir Mahal <hamirmahal@gmail.com>
2021-01-11 23:55:10 -0800

    Complete task 11. Render React class component
    This commit completes task 11. Render React class component (https://github.com/hamirmahal/build-your-own-react#11-render-react-class-component).

--- src/react-dom/VCompositeNode.js
+++ src/react-dom/VCompositeNode.js
@@ -21,15 +21,20 @@ export default class VCompositeNode {
     update() {}
 
     /**
-     * calls this VCompositeNode's
-     * React element's type, which is a
-     * React functional component, with
-     * this VCompositeNode's React
-     * element's props object passed
-     * as a parameter argument.
+     * renders, instantiates, mounts
+     * and returns a React functional
+     * component or React class component.
      * @returns {*} a mounted Node
      */
     mount() {
+        // 11. https://github.com/hamirmahal/build-your-own-react#11-render-react-class-component
+        // Extend mount() so it is capable of mounting class components.
+        if (this.reactElement.type.prototype.isReactComponent) {
+            const classComponent = new this.reactElement.type(this.reactElement.props);
+            const reactElement = classComponent.render();
+            const virtualNode = instantiateVNode(reactElement);
+            return virtualNode.mount();
+        }
         const renderedComponent = this.reactElement.type(this.reactElement.props);
         const vNode = instantiateVNode(renderedComponent);
         return vNode.mount();
